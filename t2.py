0a36e5af26ca43d94db751e7638cc37f35f1d7cf
smmr0ago <smmr0ago@gmail.com>
2024-12-18 18:29:00 +0900

    test

--- t2.py
+++ t2.py
@@ -1,2 +1,20 @@
-print('hello3')
+'''
+model : 4 -> 1 action
+DQN :
+        predicts = torch.gather(self.model(history), 1, actions)
+        max_next_q = self.target_model(next_history).detach().max(1)[0].unsqueeze(1)
+        targets = rewards + (1 - dones) * self.discount_factor * max_next_q
+
+        loss = nn.SmoothL1Loss()(predicts, targets).to(self.device)
+'''       
+
+env = gym.make("CartPole-v1", render_mode="rgb_array")
+obs = (4,     
+       
+
+
+class MyModel
+    nn 
+
+class MyAgent:
 
